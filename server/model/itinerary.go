cb940f1ce18d2fcdf26589f2c1e4368b59afdb1c
Kirill Diordiev <ikirill545@gmail.com>
2022-04-07 15:57:37 +0300

    more models

--- server/model/itinerary.go
+++ server/model/itinerary.go
@@ -1,1 +1,21 @@
 package model
+
+import "gorm.io/gorm"
+
+type ItineraryType string
+
+const (
+	UsersItinerary   ItineraryType = "USER_ITINERARY"
+	DefaultItinerary ItineraryType = "DEFAULT_ITINERARY"
+)
+
+type Itinerary struct {
+	gorm.Model
+	ID       string `gorm:"default:generated();" json:"id"`
+	name     string
+	Type     ItineraryType
+	tags     string
+	authorID string
+	fromID   string
+	toID     string
+}
