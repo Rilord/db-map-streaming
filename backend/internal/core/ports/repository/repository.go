e03f56e71242f75202348a9a8676118a5a286a17
Kirill Diordiev <ikirill545@gmail.com>
2022-04-18 18:47:10 +0300

    update repository port

--- backend/internal/core/ports/repository/repository.go
+++ backend/internal/core/ports/repository/repository.go
@@ -2,50 +2,73 @@ package ports
 
 import "game-server/internal/core/domain"
 
+type MapTileFilter struct {
+	Ids   []string
+	Limit int
+}
+
 type MapTileRepository interface {
-	Get(domain.Tile) (domain.Tile, error)
-	GetRaster(tileId string) (*domain.Raster, error)
+	Get(tile_id string) (domain.Tile, error)
+	GetRaster(tile_id string) (*domain.Raster, error)
+	GetVector(tile_id string) ([]*domain.Raster, error)
+	Find(filter MapTileFilter) ([]*domain.Tile, error)
+}
+
+type VectorObjectFilter struct {
+	Ids   []string
+	Type  int
+	Limit int
 }
 
 type VectorObjectRepository interface {
 	Add(vector domain.VectorObject) error
-	Get(domain.VectorObject) (domain.VectorObject, error)
-	Delete(domain.VectorObject) error
+	Get(vector_id string) (domain.VectorObject, error)
+	Find(filter VectorObjectFilter) ([]*domain.VectorObject, error)
+	Delete(vector_id string) error
 }
 
 type UserRepository interface {
 	Add(domain.User) error
-	Get(domain.User) (domain.User, error)
+	Get(user_id string) (domain.User, error)
 	Update(domain.User) error
-	Delete(domain.User) error
+	Delete(user_id string) error
+}
+
+type PinFilter struct {
+	Ids   []string
+	Type  string
+	Limit int
+	tags  []string
 }
 
 type PinRepository interface {
 	Add(domain.Pin) error
-	Get(domain.Pin) (domain.Pin, error)
+	Get(pin_id string) (domain.Pin, error)
+	Find(filter PinFilter) ([]*domain.Pin, error)
 	Update(domain.Pin) error
-	Delete(domain.Pin) error
+	Delete(pin_id string) error
 }
 
 type LocationRepository interface {
 	Add(domain.Location) error
-	Get(domain.Location) (domain.Location, error)
+	Get(location_id string) (domain.Location, error)
 	Update(domain.Location) error
 }
 
 type ChangeRequestRepository interface {
 	Add(domain.ChangeRequest) error
-	Get(domain.ChangeRequest) (domain.ChangeRequest, error)
+	Get(request_id string) (domain.ChangeRequest, error)
 	Delete(domain.ChangeRequest) error
 }
 
 type ArticleRepository interface {
 	Add(domain.Article) error
-	Get(domain.Article) (domain.Article, error)
-	GetPins(domain.Article) ([]domain.Pin, error)
-	GetItineraries(domain.Article) ([]domain.Itinerary, error)
-	GetComments(domain.Article) ([]domain.Comment, error)
-	Delete(domain.Article) error
+	Get(article_id string) (domain.Article, error)
+	GetPins(article_id string) ([]*domain.Pin, error)
+	GetItineraries(article_id string) ([]*domain.Itinerary, error)
+	GetChangeRequests(article_id string) ([]*domain.ChangeRequest, error)
+	GetComments(article_id string) ([]*domain.Comment, error)
+	Delete(article_id string) error
 }
 
 type ItineraryRepository interface {
@@ -55,22 +78,33 @@ type ItineraryRepository interface {
 	Delete(itinerary_id string) error
 }
 
+type RoleFilter struct {
+	Ids   []string
+	Users []string
+	Types []domain.RoleType
+}
+
 type RoleRepository interface {
-	FindUsersRole(domain.User) (domain.Role, error)
-	FIndUsersWithRole(domain.Role) ([]domain.User, error)
+	Find(filter RoleFilter) ([]*domain.User, error)
 }
 
 type LayerRepository interface {
-	Get(domain.Layer) (domain.Layer, error)
-	GetTilesAtScale(domain.VectorObject) ([]domain.Tile, error)
+	Get(layer_id string) (domain.Layer, error)
 }
 
 type RasterRepository interface {
-	Get(domain.Raster) (domain.Raster, error)
+	Get(raster_id string) (domain.Raster, error)
+}
+
+type CommentFilter struct {
+	Ids       []string
+	ArticleId string
+	CreatorId string
 }
 
 type CommentRepository interface {
 	Add(domain.Comment) error
-	Get(domain.Comment) (domain.Comment, error)
-	Delete(domain.Comment) error
+	Get(comment_id string) (domain.Comment, error)
+	Find(filter CommentFilter) ([]*domain.Comment, error)
+	Delete(comment_id string) error
 }
--- backend/internal/core/services/interactor/article/delete.go
+++ backend/internal/core/services/interactor/article/delete.go
@@ -1,1 +1,5 @@
 package article
+
+func (i *Interactor) Delete(article_id string) error {
+	return i.Article.Delete(article_id)
+}
